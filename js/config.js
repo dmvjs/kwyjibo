e8181ca1f948f76ccbc083d0a73056995d81a7d7
kirk elliott <elliott@ionq.co>
2023-04-15 01:08:31 -0400

    removes key restriction from mix

--- js/config.js
+++ js/config.js
@@ -1,10 +1,7 @@
 const config = {
-   // initialKey:12,
-   initialTempo: 94,
-    fileType: '.wav',
-    // length: 5, //17
-    // changeTempoAtIndex: 19, //4,
-    // changeTo102AtIndex: 39 //9
+    //initialKey: 4,
+    //initialTempo: 84,
+    // fileType: '.wav',
 }
 
 export {config}
